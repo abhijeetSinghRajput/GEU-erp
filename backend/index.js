81c8a03ca04bb79db8bb789613646f32adc9b8d1
Abhijeet Singh Rajput <134230962+abhijeetSinghRajput@users.noreply.github.com>
2025-08-11 02:57:11 +0530

    fix: uncomment and format root route handler for clarity

--- backend/index.js
+++ backend/index.js
@@ -26,15 +26,15 @@ app.use(
 app.use(cookieParser());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello from server" });
-});
+// app.get("/", (req, res) => {
+//   res.status(200).json({ message: "Hello from server" });
+// });
 
 app.use("/api/fee", freeRouter);
 app.use("/api/exam", examRouter);
 app.use("/api/attendance", attendanceRouter);
 app.use("/api/auth", authRouter);
-app.use("/api/", dashboardRouter);
+app.use("/api", dashboardRouter);
 
 if(process.env.NODE_ENV === "production"){
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
